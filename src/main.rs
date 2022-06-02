037fde66fb8b41330980c180c8a13498e24c9f43
Dovydas Joksas <dovydas@yoshke.org>
2022-06-02 22:52:56 +0300

    create `create_url`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use chrono::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn create_url(user_id: u32, start_time: DateTime<Utc>) -> String {
+    format!(
+        "https://api.twitter.com/2/users/{user_id}/tweets?start_time={datetime_iso_8601}",
+        user_id = user_id,
+        datetime_iso_8601 = start_time.to_rfc3339_opts(SecondsFormat::Millis, true),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_url() {
+        let user_id = 123;
+        let start_time = Utc.ymd(2022, 6, 27).and_hms(1, 30, 0);
+
+        let url = create_url(user_id, start_time);
+        assert_eq!(
+            url,
+            "https://api.twitter.com/2/users/123/tweets?start_time=2022-06-27T01:30:00.000Z"
+        );
+    }
 }
