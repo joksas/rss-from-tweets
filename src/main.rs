8b18a0ffc8a707d6c5df849c5800aaca84cd5eba
Dovydas Joksas <dovydas@yoshke.org>
2022-06-11 23:28:33 +0300

    delete commented out code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ mod server {
     use warp::Filter;
     pub async fn run(port: u16) {
         let root = warp::path::end().and_then(handlers::root);
-        // let css = warp::path("assets").and(warp::fs::dir("assets"));
         let css = warp::path("style.css")
             .map(|| include_str!("../assets/style.css"))
             .map(|reply| warp::reply::with_header(reply, "content-type", "text/css"));
