e11e725b332d45a17afd8fba1a3b37f2ddb6f9bd
Dovydas Joksas <dovydas@yoshke.org>
2022-08-11 18:08:41 +0300

    add logging

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,19 @@ use actix_web::{App, HttpServer};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(handlers::config))
-        .bind(("127.0.0.1", 3030))?
-        .run()
-        .await
+    const PORT: u16 = 8080;
+    env_logger::init();
+
+    match HttpServer::new(|| App::new().configure(handlers::config)).bind(("127.0.0.1", PORT)) {
+        Ok(server) => {
+            log::info!("Starting server at http://localhost:{}", PORT);
+            server.run().await
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    }
 }
 
 mod handlers {
