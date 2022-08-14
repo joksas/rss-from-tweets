2aee4462ab62d692386e4ff612abc790840d366f
Dovydas Joksas <dovydas@yoshke.org>
2022-08-14 20:59:28 +0300

    use Twitter API Rust library

--- src/main.rs
+++ src/main.rs
@@ -77,89 +77,35 @@ mod twitter {
     use super::secrets;
     use chrono::prelude::*;
     use reqwest::Client;
-
-    fn url_tweets(user_id: u32, start_time: DateTime<Utc>) -> String {
-        format!(
-            "https://api.twitter.com/2/users/{user_id}/tweets?start_time={datetime_iso_8601}",
-            user_id = user_id,
-            datetime_iso_8601 = start_time.to_rfc3339_opts(SecondsFormat::Millis, true),
-        )
-    }
-
-    fn url_id_from_username(username: &str) -> String {
-        format!(
-            "https://api.twitter.com/2/users/by/username/{username}",
-            username = username
-        )
-    }
+    use twitter_v2::{authorization, query, TwitterApi};
 
     async fn id_from_username(username: &str) -> Result<u64, String> {
-        let client = Client::new();
-
-        let url = url_id_from_username(username);
         let secrets = secrets::extract();
         let secrets = match secrets {
             Ok(secrets) => secrets,
             Err(err) => return Err(err),
         };
-        println!("URL: {}", url);
 
-        let request = client
-            .get(url)
-            .bearer_auth(secrets.twitter.bearer_token)
-            .build();
-        let request = match request {
-            Ok(request) => request,
-            Err(e) => return Err(e.to_string()),
+        let auth = authorization::BearerToken::new(secrets.twitter.bearer_token);
+
+        let user = match TwitterApi::new(auth)
+            .get_user_by_username(username)
+            .tweet_fields([query::TweetField::AuthorId, query::TweetField::CreatedAt])
+            .send()
+            .await
+        {
+            Ok(user) => user,
+            Err(err) => return Err(err.to_string()),
         };
 
-        let response = client.execute(request).await;
-        let response = match response {
-            Ok(response) => response,
-            Err(e) => return Err(e.to_string()),
-        };
+        let user = user.into_data().expect("this user should exist");
 
-        let response = match response.json::<serde_json::Value>().await {
-            Ok(response) => response,
-            Err(e) => return Err(e.to_string()),
-        };
-
-        let string_user_id = match response["data"]["id"].as_str() {
-            Some(user_id) => user_id,
-            None => return Err("Could not parse user_id".to_string()),
-        };
-
-        let user_id = match string_user_id.parse::<u64>() {
-            Ok(user_id) => user_id,
-            Err(e) => return Err(e.to_string()),
-        };
-
-        Ok(user_id)
+        Ok(user.id.as_u64())
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
-
-        #[test]
-        fn test_url_tweets() {
-            let user_id = 123;
-            let start_time = Utc.ymd(2022, 6, 27).and_hms(1, 30, 0);
-
-            let url = url_tweets(user_id, start_time);
-            assert_eq!(
-                url,
-                "https://api.twitter.com/2/users/123/tweets?start_time=2022-06-27T01:30:00.000Z"
-            );
-        }
-
-        #[test]
-        fn test_url_id_from_username() {
-            let user_id = "jack";
-
-            let url = url_id_from_username(user_id);
-            assert_eq!(url, "https://api.twitter.com/2/users/by/username/jack");
-        }
 
         #[tokio::test]
         async fn test_id_from_username() {
